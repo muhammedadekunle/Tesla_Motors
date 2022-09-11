ad41183f47f81e371c38eb4f22db64580882c149
Adekunle Muhammed <princeharmydex@gmail.com>
2022-09-11 19:28:32 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/*.html'
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    colors: {
+      heading: "#bada55",
+    },
+  },
+  plugins: [],
+}
